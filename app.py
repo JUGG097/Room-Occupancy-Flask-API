e79096162304297bca279c00fbbd1f054f71968d
Adeoluwa Adeboye <adeneye97@yahoo.com>
2023-04-18 09:18:40 +0100

    :construction: allow for dynamic arrangement of request body

--- app.py
+++ app.py
@@ -18,11 +18,19 @@ def health_check():
 def occupancy_prediction():
     try:
         req_body = json.loads(request.data)
-        req_body["S5_CO2_Slope"] = 0.5
-        # print(req_body)
-        df_data = pd.DataFrame(req_body, index=[0])
+        # allow for dynamic arrangement of request body
+        data = {
+            "Temp": req_body["Temp"],
+            "Light": req_body["Light"],
+            "Sound": req_body["Sound"],
+            "PIR": req_body["PIR"],
+            "Day_Period": req_body["Day_Period"],
+            "S5_CO2": req_body["S5_CO2"],
+            "S5_CO2_Slope": 0.5,
+        }
+        
+        df_data = pd.DataFrame(data, index=[0])
         pred = model.predict(df_data)
-        # print(pred)
         return {"success": True, "prediction": str(pred[0])}, 200
     except:
         return {"success": False, "error": "Internal Server Error"}, 500
